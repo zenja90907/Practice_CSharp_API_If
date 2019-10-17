4e3aa5910d5c74df20909e230bab6236a24de666
zenja90907 <55218478+zenja90907@users.noreply.github.com>
2019-10-17 12:08:56 +0800

    作業完成

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    Animator animator;
+    bool run;
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        run = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+            {
+                animator.ResetTrigger("吃飯觸發器");
+                animator.SetTrigger("吃飯觸發器");
+            }
+        if (Input.GetKey(KeyCode.R))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+        if (run == false)
+        {
+            animator.SetBool("跑步開關", false);
+        }
+        if (run == true)
+        {
+            animator.SetBool("跑步開關", true);
+        }
+    }
+}
